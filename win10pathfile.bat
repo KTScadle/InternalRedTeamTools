1f2cc872a1f7edd643f9d31aa1054a6ff7cc8149
KTScadle <83041870+KTScadle@users.noreply.github.com>
2021-05-09 23:09:16 -0700

    USBDrop
    File tree/shortcuts to .bat that runs harmless echo script in cmd.

---
+++ win10pathfile.bat
@@ -0,0 +1,135 @@
+@echo //@!USB-host-27.ru Activated //
+@echo off
+@echo *WARNING - CLOSING THIS WINDOW WILL RESULT IN USBKILL.EXE AND WILL RESULT IN CIRCUIT OVERLOAD AND PROCESSOR DESTRUCTION*
+TIMEOUT /T 4 /NOBREAK >nul 
+echo Loading Payload..50
+TIMEOUT /T 3 /NOBREAK >nul 
+echo Loading Payload..99
+TIMEOUT /T 1 /NOBREAK >nul 
+cd /d C:
+TIMEOUT /T 1 /NOBREAK >nul
+dir 
+TIMEOUT /T 3 /NOBREAK >nul 
+netstat -r
+TIMEOUT /T 1 /NOBREAK >nul
+echo Indexing Files for Uploading
+TIMEOUT /T 1 /NOBREAK >nul
+echo Indexing 10
+TIMEOUT /T 2 /NOBREAK >nul
+echo Indexing 38
+TIMEOUT /T 1 /NOBREAK >nul
+echo Indexing 54
+TIMEOUT /T 1 /NOBREAK >nul
+echo Indexing 66
+TIMEOUT /T 1 /NOBREAK >nul
+echo Indexing 78
+TIMEOUT /T 1 /NOBREAK >nul
+echo Indexing 100% Complete
+TIMEOUT /T 2 /NOBREAK >nul
+ping 8.8.8.8
+TIMEOUT /T 3 /NOBREAK >nul
+echo Exporting C:/ to host-27.pool233.60501.ru:8999;http://[2001:db8:a0b:12f0::1]:80/index.html
+echo Exporting Network Files to host-27.pool233.60501.ru:8999;http://[2001:db8:a0b:12f0::1]:80/index.html
+TIMEOUT /T 1 /NOBREAK >nul
+echo Uploading 26
+TIMEOUT /T 2 /NOBREAK >nul
+echo Uploading 78
+TIMEOUT /T 2 /NOBREAK >nul
+echo Uploading 96
+TIMEOUT /T 4 /NOBREAK >nul
+echo Success! 100% Complete
+TIMEOUT /T 3 /NOBREAK >nul
+cls
+TIMEOUT /T 1 /NOBREAK >nul
+echo Mirror Uploadoading
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60501.ru
+TIMEOUT /T 2 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60502.ru
+TIMEOUT /T 2 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60503.ru
+TIMEOUT /T 3 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60504.ru
+TIMEOUT /T 2 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60505.ru
+TIMEOUT /T 3 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60506.ru
+TIMEOUT /T 2 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 5 /NOBREAK >nul
+echo host-27.pool233.60507.ru timed out
+TIMEOUT /T 2 /NOBREAK >nul
+echo Failed
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60508.ru
+TIMEOUT /T 3 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60509.ru
+TIMEOUT /T 2 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60510.ru
+TIMEOUT /T 1 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60511.ru
+TIMEOUT /T 2 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60512.ru
+TIMEOUT /T 2 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60513.ru
+TIMEOUT /T 2 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60514.ru
+TIMEOUT /T 1 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60515.ru
+TIMEOUT /T 3 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60516.ru
+TIMEOUT /T 1 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60517.ru
+TIMEOUT /T 2 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60518.ru
+TIMEOUT /T 2 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60519.ru
+TIMEOUT /T 2 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+echo Upload to host-27.pool233.60520.ru
+TIMEOUT /T 2 /NOBREAK >nul
+echo Complete
+TIMEOUT /T 1 /NOBREAK >nul
+@echo I'm all done here, but you might want to notify your network administrator.  It's going to be a long night.....
+TIMEOUT /T 10 /NOBREAK >nul
+echo Payload Destroyed
+start chrome.exe
+start chrome.exe
+start chrome.exe
+start chrome.exe
+start chrome.exe
+TIMEOUT /T 999
+
